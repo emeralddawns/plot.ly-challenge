809da066f9c9cb815aed000e18625676d13edfab
emeralddawns <93622526+emeralddawns@users.noreply.github.com>
2022-04-01 22:56:12 -0500

    Update app.js

--- Biodiversity/static/js/app.js
+++ Biodiversity/static/js/app.js
@@ -1,5 +1,17 @@
-fetch("C:/Users/emera/Documents/GitHub/plotly-challenge/Biodiversity/samples.json")
-.then(response => {
-   return response.json();
-})
-.then(jsondata => console.log(jsondata));
+// fetch("https://github.com/emeralddawns/plotly-challenge/blob/main/Biodiversity/samples.json")
+// .then(response => {
+//    return response.json();
+// })
+// .then(jsondata => console.log(jsondata));
+
+const url = "https://github.com/emeralddawns/plotly-challenge/blob/main/Biodiversity/samples.json";
+
+// Promise Pending
+const dataPromise = d3.json(url);
+console.log("Data Promise: ", dataPromise);
+
+// Fetch the JSON data and console log it
+d3.json(url).then(function(data) {
+  console.log(data);
+});
+
