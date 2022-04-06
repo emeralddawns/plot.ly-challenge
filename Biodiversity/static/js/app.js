0c7d8720736468cf0f999bfeda3917ab733ad8d3
emeralddawns <93622526+emeralddawns@users.noreply.github.com>
2022-04-06 15:28:39 -0500

    update README

--- Biodiversity/static/js/app.js
+++ Biodiversity/static/js/app.js
@@ -1,102 +1,247 @@
 function init() {
+  var dropdownMenu = d3.select("#selDataset");
     d3.json("samples.json").then(data => {
-        console.log("read samples");
-        console.log(data);
 
-        // verify all the data that is called later
-        console.log(data.metadata[0].age);
-        console.log(data.metadata[0].wfreq);
-        console.log(data.samples[0].sample_values);
-        console.log(data.samples[0].otu_ids);
-        console.log(data.samples[0].otu_labels);
+        // Fill out dropdown menu
+        sampleID = data.names;
+        //for each loop... for each i in the list found in data.names (aka sampleID), append the value (ex: 940), as text, to the bottom of the "options" in the dropdown list
+        sampleID.forEach((i)=>{dropdownMenu.append("option").text(i).property("value");
+        })
 
-        // testBar should only display top 10, needs labels
-        let testBar = [{
-            type: "bar",
-            x: data.samples[0].sample_values,
-            y: data.samples[0].otu_ids,
-            text: data.samples[0].otu_labels,
-            orientation: 'h'
-        }];
+        demographics(sampleID[0]);
+        charts(sampleID[0]);
+        gauge(sampleID[0]);
+    })};
+
+function demographics(userInput) {
+    d3.json("samples.json").then(data => {
+
+        // The demographics info is in data.metadata
+        // meta is the list of all metadata dictionaries
+        meta = data.metadata;
+
+        //filter... within meta (aka data.metadata) match record.ID (record is a single dictionary in the metadata list of dictionaries) to the userInput
+        //selected is a all records (dictionaries) that match the ID of the userInput, of which there should only be one
+        selected = meta.filter((record)=>record.id == userInput);
+
+        //firstID is the first dictionary in the list of matching records, of which there should only have been one
+        firstID = selected[0];
+
+        var metaBox = d3.select("#sample-metadata");
+
+        //A json dictionary is a js object. Object.entries() "returns an array of a given object's own enumerable string-keyed property [key, value] pairs".
+        // select the div with id(#) "sample-metadata" (aka metaBox) and append the key: value pairs in the given text format
+        Object.entries(firstID).forEach(([key, value])=>{metaBox.append("option").text(`${key}: ${value}`);
+    })
+    })
+};
+
+function charts (userInput){
+  d3.json("samples.json").then(data => {
+
+    chartSamples = data.samples;
+    selected = chartSamples.filter((record)=>record.id == userInput);
+    firstID = selected[0];
+
+    //BAR CHART
+    let barChart = [{
+        type: "bar",
+        x: firstID.sample_values.slice(0,10).reverse(),
+        y: firstID.otu_ids.map(j=>`otu ${j}`).slice(0,10).reverse(),
+        text: firstID.otu_labels.slice(0,10).reverse(),
+        orientation: 'h'
+    }];
+
+    let barLayout = {
+      title: { text: `Top 10 OTUs for Sample ${userInput}` },
+      margin: {
+        t: 23,
+      },
+      xaxis: {
+        title: {
+          text: "Read Counts"
+        }
+      },
+      yaxis: {
+        title: {
+          text: "OTU IDs"
+        }
+      }
+    };
+
+    let barConfig = {responsive: true}
+
+    Plotly.newPlot("bar", barChart, barLayout, barConfig);
+
+    //BUBBLE CHART
+    let bubbleChart = [{
+        x: firstID.otu_ids,
+        y: firstID.sample_values,
+        text: firstID.otu_labels,            
+        mode: 'markers',
+        marker: {
+          color: firstID.otu_ids,
+          colorscale: "Portland",
+          size: firstID.sample_values
+        }
         
-          Plotly.newPlot("bar", testBar);
+    }];
+    let bubbleLayout = {
+      dragmode: "pan",
+      title: { text: `OTU Amounts for Sample ${userInput}` },
 
-        // testGauge
-        let testGauge = [{
-          domain: { x: [0, 1], y: [0, 1] },
-          value: data.metadata[0].wfreq,
-          title: { text: "Wash Frequency" },
-          type: "indicator",
-          mode: "gauge+number",
-          gauge: {
-            axis: { range: [null, 9] },
-            steps: [
-              { range: [0, 5], color: "lightgray" },
-              { range: [5, 9], color: "gray" }
-            ]}
+      xaxis: {
+        title: {
+          text: 'OTU ID'
+        }},
+      yaxis: {
+        title: {
+          text: "Read Counts"
+        }
+      }
 
-        }];
+    }
+    let bubbleConfig = {responsive: true, scrollZoom: true};
 
-        Plotly.newPlot("gauge", testGauge);
+    Plotly.newPlot("bubble", bubbleChart, bubbleLayout, bubbleConfig);
 
-        //testBubble needs labels
-        let testBubble = [{
-            x: data.samples[0].otu_ids,
-            y: data.samples[0].sample_values,
-            text: data.samples[0].otu_labels,            
-            mode: 'markers',
-            marker: {
-              color: data.samples[0].otu_ids,
-              size: data.samples[0].sample_values
-            }
+  }
+)};
+        
+function gauge (userInput){
+  d3.json("samples.json").then(data => {
 
-        }];
+    gaugeInfo = data.metadata;
+    selected = gaugeInfo.filter((record)=>record.id == userInput);
+    firstID = selected[0];
+    console.log(firstID); 
 
-            Plotly.newPlot("bubble", testBubble);
+    //colors from: https://hihayk.github.io/scale/#4/6/50/80/-51/67/20/14/1D9A6C/29/154/108/white
+    let colors = [
+      "#137177",
+      "#188977",
+      "#1D9A6C",
+      "#39A96B",
+      "#56B870",
+      "#74C67A",
+      "#99D492",
+      "#BFE1B0",
+      "#DEEDCF"]
+   
+    // Gauge using "Indicator" Type - (matching the instructions) edited from https://plotly.com/javascript/gauge-charts/
+    let indicatorGraph = [{
+      domain: { x: [0, 1], y: [0, 1] },
+      value: firstID.wfreq,
+      title: { text: "<b>Wash Frequency</b><br><i>Scrubs per Week</i>" },
+      type: "indicator",
+      mode: "gauge+number",
+      gauge: {
+        axis: { range: [null, 9],
+          tickmode: "linear",
+          ticks: "outside" },
+        steps: [
+          { range: [0, 1], color: colors[8] },
+          { range: [1, 2], color: colors[7] },
+          { range: [2, 3], color: colors[6] },
+          { range: [3, 4], color: colors[5] },
+          { range: [4, 5], color: colors[4] },
+          { range: [5, 6], color: colors[3] },
+          { range: [6, 7], color: colors[2] },
+          { range: [7, 8], color: colors[1] },
+          { range: [8, 9], color: colors[0] },
+        ],
+        bar: {color: "#0A2F51"}
+      },
+    }];
+
+    let indicatorConfig = {
+      responsive: true,
+      autosize: true,
+      hovermode: "closest"};
+
+    Plotly.newPlot("indicator", indicatorGraph, indicatorConfig);
 
 
+    //Gauge using "Pie" type - (matching the picture) edited from https://codepen.io/plotly/pen/rxeZME
+    var level = firstID.wfreq * 20;
 
-         landingTable = data.metadata   
-         Plotly.newPlot("sample-metadata", landingTable);
-    });
+    // Trig to calc meter point
+    var degrees = 180 - level,
+       radius = .5;
+    var radians = degrees * Math.PI / 180;
+    var x = radius * Math.cos(radians);
+    var y = radius * Math.sin(radians);
+    
+    // Path: may have to change to create a better triangle
+    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+       pathX = String(x),
+       space = ' ',
+       pathY = String(y),
+       pathEnd = ' Z';
+    var path = mainPath.concat(pathX,space,pathY,pathEnd);
+    
+    var pieGraph = [{ type: 'scatter',
+       x: [0], y:[0],
+      marker: {size: 28, color:'850000'},
+      showlegend: false,
+      name: 'wash frequency',
+      text: firstID.wfreq,
+      hoverinfo: 'text+name'},
+      { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
+      rotation: 90,
+      text: ['8-9', '7-8', '6-7', '5-6',
+          '4-5', '3-4', '2-3', '1-2', '0-1', ''],
+      textinfo: 'text',
+      textposition:'inside',	  
+      marker: {colors:[ colors[0], colors[1], colors[2], colors[3], colors[4], colors[5], colors[6], colors[7], colors[8], "rgba(255, 255, 255, 0)"]},
+      labels: ['8-9', '7-8', '6-7', '5-6',
+      '4-5', '3-4', '2-3', '1-2', '0-1',''],
+      hoverinfo: 'label',
+      hole: .5,
+      type: 'pie',
+      showlegend: false
+    }];
+    
+    var pieLayout = {
+      shapes:[{
+          type: 'path',
+          path: path,
+          fillcolor: '850000',
+          line: {
+            color: '850000'
+          }
+        }],
+      title: "<b>Wash Frequency</b><br><i>Scrubs per Week</i>",
+      height: 650,
+      width: 650,
+      xaxis: {zeroline:false, showticklabels:false,
+           showgrid: false, range: [-1, 1]},
+      yaxis: {zeroline:false, showticklabels:false,
+           showgrid: false, range: [-1, 1]},
+      margin: {
+      b: -30,
+      }
+    };
 
-}
+    var pieConfig = {
+      responsive: true,
+      autosize: true,
+      hovermode: "closest"};
 
-
-
-// Call updatePlotly() when a change takes place to the DOM
-d3.selectAll("#selDataset").on("change", optionChanged);
-
+    Plotly.newPlot("pie", pieGraph, pieLayout, pieConfig);
+  })
+};
 // This function is called when a dropdown menu item is selected
 function optionChanged(value) {
-    // Use D3 to select the dropdown menu
-    var dropdownMenu = d3.select("#selDataset");
-    // Assign the value of the dropdown menu option to a variable
-    var dataset = dropdownMenu.property("value");
-    
+   
+    //empty the "Demographic Info" box
+    var metaBox = d3.select("#sample-metadata");
+    metaBox.selectAll("*").remove();
 
-    // PLACEHOLDER: Replace with real charts and data
+    demographics(value);
+    charts(value);
+    gauge(value);
 
-    // Initialize x and y arrays
-    var x = [];
-    var y = [];
+};
 
-    if (dataset === '940') {
-        x = [1, 2, 3, 4, 5];
-        y = [1, 2, 4, 8, 16];
-      }
-    
-      else if (dataset === '941') {
-        x = [10, 20, 30, 40, 50];
-        y = [1, 10, 100, 1000, 10000];
-      }
-
-    // Note the extra brackets around 'x' and 'y'
-    Plotly.restyle("bar", "x", [x]);
-    Plotly.restyle("bar", "y", [y]);
-
-
-    console.log(value);
-}
-    
 init();
--- README.md
+++ README.md
@@ -6,17 +6,28 @@ The code uses json information on Belly Button Biodiversity from [The Public Sci
 
 # What the code displays
 
-The **app.js** code uses d3, plot.ly, and **samples.json** to create 4  graphs and a drop-down list that allows users to choose between samples. The information is displayed in the following:
+The **app.js** code uses d3, plot.ly, and **samples.json** to create 4 graphs, a list of sample information, and a drop-down list that allows users to choose between samples. The information is displayed in the following:
     
-    1. A demographic information list containing the id, ethnicity, gender, age, location, bbtype, and wash frequency for the contributor of each sample.
+    1. A demographic information list containing the id, ethnicity, gender, age, location, belly button type (innie vs outie), and wash frequency for the contributor of each sample.
 
     2. A horizontal bar chart that displays the sample values of the top ten (10) operational taxonomic units (OTUs) found in the sample.
 
-    3. A gauge chart visually displaying the belly button wash frequency reported by the sample contributor.
+    3. Two gauge charts visually displaying the belly button wash frequency reported by the sample contributor. The first uses the challenge instructions to format and display (using type: 'indicator'), and the second more closely resembles the example gauge in the pictures included with the challenge instructions (using type: 'pie').
 
     4. A bubble chart that displays the sample values of all of the OTUs.
 
-The **index.html**, **style.css**, and Bootstrap 3 are used to create the webpage displaying the graphs and information.
+**index.html**, **style.css**, and Bootstrap 3 are used to create the webpage displaying the graphs and information.
 
-A screenshot of the webpage can be seen below:
-![Mars_Info_Scraper.png](Mars_Info_Scraper.png)
+# Notes on creating gauge charts using plot.ly type: "indicator" vs type: "pie"
+
+    1. Indicators do not seem to resize in real-time even when responsive is set to "true". This can result in parts of the chart being cut off when the page size is changed without refreshing the page. They do still auto-size on page open. 
+
+    2. Pie charts that have the vertically lowest segment set to zero (0) opacity in order to look like a gauge still take up as much vertical space as they would otherwise. This results in what looks to the viewer like a large amount of blank space.
+
+    3. Plot.ly shapes added to graphs can be zoomed in on, even if the parent graph can't. This can cause the graph to look "broken". Disabling zoom on the graph resulted in a needle that was no longer centered on the pie chart, but on the bottom right of the plot-area.
+
+    4. The origin for indicators is in the bottom left corner of the graph. The origin for pie charts is in the center of the chart. Drawing gauge needle shapes has to take this into account. 
+
+
+A screenshot of the dashboard can be seen below:
+![bbdashboard.png](bbdashboard.png)
